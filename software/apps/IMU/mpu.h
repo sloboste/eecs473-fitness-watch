1768a03cab73e156e0dcd4cdb37c645338c96718
amitshah326 <amitshah@umich.edu>
2015-11-01 20:37:44 -0500

    Untested IMU code

---
+++ software/apps/IMU/mpu.h
@@ -0,0 +1,16 @@
+#ifndef MPU_H
+#define MPU_H
+
+struct s_mympu {
+	float ypr[3];
+	float gyro[3];
+	unsigned long steps[1];
+};
+
+extern struct s_mympu mympu;
+
+int mympu_open(unsigned int rate);
+int mympu_update();
+void tap_detect(unsigned char direction, unsigned char count);
+#endif
+
