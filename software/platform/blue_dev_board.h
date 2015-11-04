84d0ae4877e3881f03564029720a7ad51acde62f
sloboste <sloboste@umich.edu>
2015-11-03 22:37:55 -0500

    Add common #defines for blue dev board.

---
+++ software/platform/blue_dev_board.h
@@ -0,0 +1,18 @@
+#ifndef BLUE_DEV_BOARD_H
+#define BLUE_DEV_BOARD_H
+
+#include "nrf_gpio.h"
+
+#define PIN_BUTTON_0 17
+#define PIN_BUTTON_1 18
+#define PIN_BUTTON_2 19
+#define PIN_BUTTON_3 20
+
+#define PIN_LED_1 21
+#define PIN_LED_2 22 
+#define PIN_LED_3 23 
+#define PIN_LED_4 24
+
+#define BUTTON_PULL GPIO_PIN_CNF_PULL_Pullup
+
+#endif
