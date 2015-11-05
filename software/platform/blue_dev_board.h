4c923006d6151c3d25620ac28f3a6e4c6e9a5893
sloboste <sloboste@umich.edu>
2015-11-04 21:36:18 -0500

    Add UART pins to blue_dev_board defines.

--- software/platform/blue_dev_board.h
+++ software/platform/blue_dev_board.h
@@ -3,16 +3,24 @@
 
 #include "nrf_gpio.h"
 
+// Push button switches
 #define PIN_BUTTON_0 17
 #define PIN_BUTTON_1 18
 #define PIN_BUTTON_2 19
 #define PIN_BUTTON_3 20
 
+// LEDs
 #define PIN_LED_1 21
 #define PIN_LED_2 22 
 #define PIN_LED_3 23 
 #define PIN_LED_4 24
 
-#define BUTTON_PULL GPIO_PIN_CNF_PULL_Pullup
+// UART
+#define RX_PIN_NUMBER 11
+#define TX_PIN_NUMBER 9
+//#define RTS_PIN_NUMBER 0 // Doesn't matter, not used 
+//#define CTS_PIN_NUMBER 0 // Doesn't matter, not used
+
+//#define BUTTON_PULL GPIO_PIN_CNF_PULL_Pullup
 
 #endif
