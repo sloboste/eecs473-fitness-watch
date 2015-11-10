f95deab33d780c75b8732d977549b1df63ffbe86
amitshah326 <amitshah@umich.edu>
2015-11-10 01:17:47 -0500

    IMU interface

--- software/apps/IMU/mpu.c
+++ software/apps/IMU/mpu.c
@@ -11,62 +11,77 @@
 #define DMP_INT_CONTINUOUS  (0x02)
 
 unsigned int tap_count = 0;
-unsigned short *rate;
 unsigned long time = 0;
-short *status;
-
-static int ret;
-static unsigned char fifoCount;
 
 
-void tap_detect(unsigned char direction, unsigned char count){
+/*void tap_detect(unsigned char direction, unsigned char count){
 	tap_count += count;
 	SEGGER_RTT_printf(0, "TAP DETECTED%d\r\n", tap_count);
 	return;
-}
+}*/
 
+//run once initially, make rate 200 always
 int mympu_open(unsigned int rate) {
   	mpu_select_device(0);
    	mpu_init_structures();
-	ret = mpu_init(NULL);
-	ret = mpu_set_sensors(INV_XYZ_GYRO|INV_XYZ_ACCEL); 
-    mpu_get_power_state((unsigned char *)&ret);
-	ret = dmp_load_motion_driver_firmware();
-	ret = dmp_set_fifo_rate(rate);
-	ret = mpu_set_dmp_state(1);
-	ret = dmp_enable_feature(DMP_FEATURE_PEDOMETER|DMP_FEATURE_TAP);
-	ret = dmp_register_tap_cb(&tap_detect);
-	ret = dmp_set_interrupt_mode(DMP_INT_GESTURE);
-
+	mpu_init(NULL);
+	mpu_set_sensors(INV_XYZ_GYRO|INV_XYZ_ACCEL); 
+    //mpu_get_power_state((unsigned char *)&ret);
+	dmp_load_motion_driver_firmware();
+	dmp_set_fifo_rate(rate);
+	mpu_set_dmp_state(1);
+	dmp_enable_feature(DMP_FEATURE_PEDOMETER|DMP_FEATURE_TAP);
+	//dmp_register_tap_cb(&tap_detect);
+	dmp_set_interrupt_mode(DMP_INT_GESTURE);
 	return 0;
 }
 
-unsigned long get_steps(){
-	unsigned long steps = dmp_get_pedometer_step_count();
-	SEGGER_RTT_printf(0, "Steps: %d\r\n", steps);
-	return steps;
+uint32_t get_steps(){
+	unsigned long steps = 0;
+	dmp_get_pedometer_step_count(&steps)
+	return (uint32_t)steps;
 }
 
-int mympu_update() {
-	//SEGGER_RTT_printf(0, ":%d\r\n", tap_count);
+void reset_steps(){
+	//try a few times to reset even if race condition occurs
+	dmp_set_pedometer_step_count(0);
+	dmp_set_pedometer_step_count(0);
+	dmp_set_pedometer_step_count(0);
+	return;
+}
 
-	//mpu_get_int_status(status);
-	//SEGGER_RTT_printf(0, "RET: %d\r\n", *status);
-
-	// do {
-	// 	ret = dmp_read_fifo(&fifoCount);
-	// 	 // will return:
-	// 		// 0 - if ok
-	// 		// 1 - no packet available
-	// 		// 2 - if BIT_FIFO_OVERFLOWN is set
-	// 		// 3 - if frame corrupted
-	// 	 //       <0 - if error
-	// 	//SEGGER_RTT_printf(0, "RET: %d\r\n", ret);
-	// 	if (ret!=0) return ret; 
-	// } while (fifoCount>1);
+uint32_t get_walktime(){
+	unsigned long time = 0;
 	dmp_get_pedometer_walk_time(&time);
-	SEGGER_RTT_printf(0, "RET: %d\r\n", time);
-	nrf_delay_ms(3000); //wait 3 seconds
-	return 0;
+	return (uint32_t)time;	
 }
+
+void reset_walktime(){
+	dmp_set_pedometer_walk_time(0);
+	dmp_set_pedometer_walk_time(0);
+	dmp_set_pedometer_walk_time(0);
+	return;	
+}
+
+// int mympu_update() {
+// 	//SEGGER_RTT_printf(0, ":%d\r\n", tap_count);
+
+// 	//mpu_get_int_status(status);
+// 	//SEGGER_RTT_printf(0, "RET: %d\r\n", *status);
+
+// 	// do {
+// 	// 	ret = dmp_read_fifo(&fifoCount);
+// 	// 	 // will return:
+// 	// 		// 0 - if ok
+// 	// 		// 1 - no packet available
+// 	// 		// 2 - if BIT_FIFO_OVERFLOWN is set
+// 	// 		// 3 - if frame corrupted
+// 	// 	 //       <0 - if error
+// 	// 	//SEGGER_RTT_printf(0, "RET: %d\r\n", ret);
+// 	// 	if (ret!=0) return ret; 
+// 	// } while (fifoCount>1);
+// 	//dmp_get_pedometer_walk_time(&time);
+// 	//nrf_delay_ms(3000); //wait 3 seconds
+// 	return 0;
+// }
 
--- software/apps/IMU/mpu.h
+++ software/apps/IMU/mpu.h
@@ -1,11 +1,23 @@
 #ifndef MPU_H
 #define MPU_H
 
-//extern unsigned long steps;
+//DONT FORGET TO CHOOSE PINS IN nrf_drv_Config
 
+//Run this at the start. Rate should be 200
 int mympu_open(unsigned int rate);
-int mympu_update();
-unsigned long get_steps();
-void tap_detect(unsigned char direction, unsigned char count);
+
+//returns number of steps
+uint32_t get_steps();
+
+//will most likely set steps to 0
+void reset_steps();
+
+//returns walk time in ms
+uint32_t get_walktime();
+
+//will most likely set walk time to 0
+void reset_walktime();
+
+//void tap_detect(unsigned char direction, unsigned char count);
 #endif
 
