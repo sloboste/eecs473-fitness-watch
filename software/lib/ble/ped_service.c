af148fa40ab41bbd0a7892f7c41bbeb036dd4315
sloboste <sloboste@umich.edu>
2015-11-11 15:00:57 -0500

    Clean up the pedometer service code.

--- software/lib/ble/ped_service.c
+++ software/lib/ble/ped_service.c
@@ -14,20 +14,21 @@ static ble_ped_t ble_ped_object;
 
 // Step count characteristic
 #define STEP_COUNT_LEN 4
-static char step_count_buf[STEP_COUNT_LEN] = {0x01, 0x02, 0x03, 0x04};
-static char * step_count_name = "Ped Step Count";
+static char step_count_buf[STEP_COUNT_LEN];
+static char * step_count_name = "Pedometer Step Count";
 static bool step_count_notifications_enabled = false; 
 
 // Status characteristic
 #define STATUS_LEN 1
-static char status_buf[STATUS_LEN] = {0x0F};
-static char * status_name = "PED Status";
+static char status_buf[STATUS_LEN];
+static char * status_name = "Pedometer Status";
 
 
 uint32_t ble_ped_update_step_count(uint32_t step_count)
 {
     uint32_t err_code = NRF_SUCCESS;
 
+    // Build a characteristic value
     ble_gatts_value_t gatts_value;
     memset(&gatts_value, 0, sizeof(gatts_value));
     
@@ -35,6 +36,7 @@ uint32_t ble_ped_update_step_count(uint32_t step_count)
     gatts_value.offset = 0;
     gatts_value.p_value = (uint8_t *) &step_count; 
     
+    // Set the new characteristic value 
     err_code = sd_ble_gatts_value_set(
         ble_ped_object.conn_handle,
         ble_ped_object.step_count_char_handles.value_handle,
@@ -44,7 +46,7 @@ uint32_t ble_ped_update_step_count(uint32_t step_count)
         return err_code;
     }
 
-    // Send notification
+    // Send notification if needed
     if (ble_ped_object.conn_handle != BLE_CONN_HANDLE_INVALID) {
         ble_gatts_hvx_params_t hvx_params;
         memset(&hvx_params, 0, sizeof(hvx_params));
@@ -66,6 +68,7 @@ uint32_t ble_ped_update_status(uint8_t status)
 {
     uint32_t err_code = NRF_SUCCESS;
 
+    // Build a characteristic value
     ble_gatts_value_t gatts_value;
     memset(&gatts_value, 0, sizeof(gatts_value));
     
@@ -73,6 +76,7 @@ uint32_t ble_ped_update_status(uint8_t status)
     gatts_value.offset = 0;
     gatts_value.p_value = (uint8_t *) &status; 
     
+    // Set the new characteristic value
     err_code = sd_ble_gatts_value_set(
         ble_ped_object.conn_handle,
         ble_ped_object.status_char_handles.value_handle,
@@ -82,7 +86,7 @@ uint32_t ble_ped_update_status(uint8_t status)
         return err_code;
     }
 
-    // Send notification
+    // Send notification if needed
     if (ble_ped_object.conn_handle != BLE_CONN_HANDLE_INVALID) {
         ble_gatts_hvx_params_t hvx_params;
         memset(&hvx_params, 0, sizeof(hvx_params));
@@ -99,30 +103,36 @@ uint32_t ble_ped_update_status(uint8_t status)
     return err_code;
 }
 
+/**
+ * Handle the event when the client connects to this device.
+ */
 static void on_connect(ble_evt_t * evt_ptr)
 {
     ble_ped_object.conn_handle = evt_ptr->evt.gap_evt.conn_handle;
 }
 
+/**
+ * Handle the event when the client disconnects from this device.
+ */
 static void on_disconnect(ble_evt_t * evt_ptr)
 {
     ble_ped_object.conn_handle = BLE_CONN_HANDLE_INVALID;
 }
 
-static void on_write(ble_evt_t * evt_ptr)
+// We don't need to do anything for a write. If you want to change that though,
+// use this as an example.
+/*static void on_write(ble_evt_t * evt_ptr)
 {
-    /* TODO: do we even need this?
     ble_gatts_evt_write_t * evt_write_ptr = &evt_ptr->evt.gatts_evt.params.write;
 
     if ((evt_write_ptr->handle == ble_ped_object.location_char.cccd_handle) &&
         (evt_write_ptr->len == 2)) {
         // Call event handler because cccd written
         if (ble_ped_object->evt_handler != NULL) {
-             
+            // Do stuff
         } 
     }
-    */
-}
+}*/
 
 void ble_ped_on_ble_evt(ble_evt_t * evt_ptr)
 {
@@ -139,6 +149,19 @@ void ble_ped_on_ble_evt(ble_evt_t * evt_ptr)
     }
 }
 
+/**
+ * Add a custom characteristic to the pedometer service.
+ *
+ * uuid -- the 16 bit UUID for the charactersitic to combine with the 128 bit   
+ *         service UUID.                                                        
+ * value_len -- length in bytes of the characteristic value.                    
+ * name -- char array that is the name to display for this characteristic.      
+ * read -- does the client have read access to this characteristic? Boolean.    
+ * write -- does the client have write access to this characteristic? Boolean.  
+ * notify -- does this characteristic support notifications? Boolean.           
+ * char_handles_ptr -- the ble_gatts_char_handles_t object dedicated to store   
+ *                     this characteristic's attribute handles. 
+ */
 static uint32_t char_add(uint16_t uuid, uint16_t value_len, char * value_buf,
                          uint8_t name_len, char * name, uint8_t read,
                          uint8_t write, uint8_t notify,
@@ -146,29 +169,35 @@ static uint32_t char_add(uint16_t uuid, uint16_t value_len, char * value_buf,
 {
     uint32_t err_code;
     
+    // Setup the characteristic UUID with the softdevice.
     ble_uuid_t char_uuid;
     ble_uuid128_t base_uuid = ble_ped_BASE_UUID;
     char_uuid.uuid = uuid;
     err_code = sd_ble_uuid_vs_add(&base_uuid, &char_uuid.type);
     
+    // Setup the characteristic value attribute metadata
     ble_gatts_attr_md_t attr_md;
     memset(&attr_md, 0, sizeof(attr_md));
     attr_md.vloc = BLE_GATTS_VLOC_STACK;
     BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
     BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&attr_md.write_perm);
 
+    // Setup the client characteristic configuration descriptor attribute
+    // metadata. 
     ble_gatts_attr_md_t cccd_md;
     memset(&cccd_md, 0, sizeof(cccd_md));
     cccd_md.vloc = BLE_GATTS_VLOC_STACK;
     BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
     BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
 
+    // Setup the user description characteristic attribute metadata.
     ble_gatts_attr_md_t desc_md;
     memset(&desc_md, 0, sizeof(desc_md));
     desc_md.vloc = BLE_GATTS_VLOC_STACK;
     BLE_GAP_CONN_SEC_MODE_SET_OPEN(&desc_md.read_perm);
     BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&desc_md.write_perm);
-
+    
+    // Setup the characteristic attribute metadata
     ble_gatts_char_md_t char_md;
     memset(&char_md, 0, sizeof(char_md));
     char_md.char_props.read = read;
@@ -180,20 +209,22 @@ static uint32_t char_add(uint16_t uuid, uint16_t value_len, char * value_buf,
     char_md.p_user_desc_md = &desc_md;
     char_md.p_cccd_md = &cccd_md;
 
+    // Create the characteristic value attribute
     ble_gatts_attr_t attr_char_value;
     memset(&attr_char_value, 0, sizeof(attr_char_value));
     attr_char_value.p_uuid = &char_uuid;
     attr_char_value.p_attr_md = &attr_md;
     attr_char_value.init_len  = value_len;
     attr_char_value.init_offs = 0;
-    attr_char_value.max_len   = value_len; // TODO: possible error when len > 512 ??
+    // NOTE: possible error when value_len > 512 ??
+    attr_char_value.max_len   = value_len;
     attr_char_value.p_value   = value_buf;
     
+    // Add the characteristic we just built
     err_code = sd_ble_gatts_characteristic_add(
         ble_ped_object.service_handle, &char_md, &attr_char_value,
         char_handles_ptr);
     
-
     return err_code;
 }
 
@@ -201,21 +232,25 @@ uint32_t ble_ped_init(void)
 {
     uint32_t err_code;
 
+    // Register the service UUID with the softdevice
     ble_uuid_t service_uuid;
     ble_uuid128_t base_uuid = ble_ped_BASE_UUID;
     service_uuid.uuid = ble_ped_UUID;
     err_code = sd_ble_uuid_vs_add(&base_uuid, &service_uuid.type);
 
+    // Register the service with the softdevice
     err_code = sd_ble_gatts_service_add(
         BLE_GATTS_SRVC_TYPE_PRIMARY,
         &service_uuid,
         &(ble_ped_object.service_handle));
 
+    // Register the step count characteristic with the softdevice
     err_code = char_add(
         ble_ped_UUID_STEP_COUNT, STEP_COUNT_LEN, &step_count_buf,
         strlen(step_count_name), step_count_name, 1, 0, 1,
         &ble_ped_object.step_count_char_handles);
 
+    // Register the status characteristic with the softdevice
     err_code = char_add(
         ble_ped_UUID_STATUS, STATUS_LEN, &status_buf,
         strlen(status_name), status_name, 1, 0, 1,
--- software/lib/ble/ped_service.h
+++ software/lib/ble/ped_service.h
@@ -1,4 +1,5 @@
-/*  Configuration/implementation for a BLE custom pedometer service.
+/**
+ * Configuration/implementation for a BLE custom pedometer service.
  */
 
 #ifndef __BLE_PED_H
@@ -12,8 +13,9 @@
 #include "ble_srv_common.h"
 #include "ble_gatts.h"
 
-
+// Following is a random UUID for the pedometer service.
 // 68c1XXXX-801a-11e5-8bcf-feff819cdc9f
+// Replace the Xs with the 16 bit UUIDs for each characteristic.
 #define ble_ped_BASE_UUID {0x9f, 0xdc, 0x9c, 0x81, 0xff, 0xfe, 0xcf, 0x8b, 0xe5, 0x11, 0x1a, 0x80, 0x00, 0x00, 0xc1, 0x68}
 #define ble_ped_UUID 0x0001
 #define ble_ped_UUID_STEP_COUNT 0x0002
@@ -46,16 +48,24 @@ struct ble_ped_struct {
     ble_ped_evt_handler_t evt_handler; // TODO we might not need this
 };
 
-// Initialize the ped service
+/**
+ * Set up the pedometer service.
+ */
 extern uint32_t ble_ped_init(void);
 
-// Call whenever a BLE stack event is received by the application 
+/**
+ * Call whenever a BLE stack event is received by the application 
+ */
 extern void ble_ped_on_ble_evt(ble_evt_t * evt_ptr);
 
-// Call to update step_count characteristic
+/**
+ * Update step_count characteristic value.
+ */ 
 extern uint32_t ble_ped_update_step_count(uint32_t step_count);
 
-// Call to update status characteristic
+/** 
+ * Update status characteristic value.
+ */
 extern uint32_t ble_ped_update_status(uint8_t status);
 
 #endif
