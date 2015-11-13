baf2c43273a3da52a33d9bdf7c7720be37918fad
sloboste <sloboste@umich.edu>
2015-11-13 01:52:35 -0500

    Add gps to demo app. So getting data from the gps causes problems... But at least we can load the GPS driver without RAM overflowing into stacks space! The gps driver bug is my fault :(

--- software/apps/watch/main.c
+++ software/apps/watch/main.c
@@ -16,27 +16,28 @@
 #include "scheduler_config.h"
 
 #include "ble_config.h"
+#include "ped_service.h"
 /*
 #include "heart_rate_service.h"
 #include "battery_service.h"
 #include "gps_service.h"
-#include "ped_service.h"
 */
 
 #include "mpu.h"
 //#include "inv_mpu.h" // FIXME remove
 //#include "SEGGER_RTT.h" // FIXME remove
 
-//#include "gps.h"
+#include "gps.h"
 
 #include "spi_driver.h"
 #include "lcd_driver.h"
 
-#include "globals.h"
+#include "app_util_platform.h" // CRITICAL SECTION
 #include "time_keeper.h"
 
 
 static rtc_time_t time;
+static gps_info_t gps_info;
 
 /**
  * Handle when an error ocurrs during runtime.
@@ -65,18 +66,51 @@ void gpio_init(void)
     nrf_gpio_pin_set(PIN_LED_4);
 }
 
+static void set_time(uint8_t hours, uint8_t minutes, uint8_t seconds)
+{
+    CRITICAL_REGION_ENTER();                                                    
+    time.seconds = seconds;
+    time.minutes = minutes;
+    time.hours = hours;
+    CRITICAL_REGION_EXIT();                                                     
+}
+
+static void increment_time()
+{                                                 
+    CRITICAL_REGION_ENTER();                                                    
+    ++time.seconds;                                                          
+    if (time.seconds == 60) {                                              
+        ++time.minutes;                                                    
+        time.seconds = 0;                                                  
+    }                                                                           
+    if (time.minutes == 60) {                                              
+        ++time.hours;                                                      
+        time.minutes = 0;                                                  
+    }                                                                           
+    if (time.hours == 24) {                                                
+        time.seconds = 0;                                                  
+        time.minutes = 0;                                                  
+        time.hours = 0;                                                    
+        time.milli = 0;                                                    
+    }                                                                           
+    CRITICAL_REGION_EXIT();                                                     
+}
 
 static app_timer_id_t timer_id_1hz;
 
 void task_1hz(void * arg_ptr)
 {
+    static int i = 0;
     static uint32_t step_count = 0;
     //static uint8_t battery_level = 100;
     //static uint16_t heart_rate_bpm = 1000;
 
     nrf_gpio_pin_toggle(PIN_LED_1);
 
-    time = rtc_get_time();
+    //gps_get_info(&gps_info, GPS_TYPE_GPRMC);
+    //set_time(gps_info.hours, gps_info.minutes, gps_info.seconds);
+
+    increment_time();
     step_count = get_steps();
     buildWatchFace_LCD(&time, get_steps());
     refresh();
@@ -86,6 +120,7 @@ void task_1hz(void * arg_ptr)
     bas_update(battery_level--);
     hrs_update(heart_rate_bpm++);
     */
+    
 }
 
 
@@ -114,16 +149,18 @@ int main(void)
     // Init BLE
     ble_init();
 
-    // Init real time counter FIXME breaks ble?
-    //rtc_config();
-    //rtc_start();
-
     // Init IMU
     mympu_open(200);
 
     // Init LCD
     spi_init();
     clearDisplay();
+    
+    // Init GPS
+    gps_init();
+    gps_config();
+    gps_enable();
+    //gps_get_info(&gps_info, GPS_TYPE_GPRMC);
 
     // TODO move this
     app_timer_create(&timer_id_1hz, APP_TIMER_MODE_REPEATED, task_1hz);
@@ -136,7 +173,7 @@ int main(void)
     // Main loop
     while (1) {
         app_sched_execute();
-        //sd_app_evt_wait();         
-        __WFI(); // FIXME remove when using ble
+        sd_app_evt_wait();         
+        //__WFI(); // FIXME remove when using ble
     }
 }
