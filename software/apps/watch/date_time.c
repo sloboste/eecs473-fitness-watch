18f004f35c35b7d228053377e9e507ad02aa1192
sloboste <sloboste@umich.edu>
2015-11-23 12:46:24 -0500

    Use table to simply date_time code

--- software/apps/watch/date_time.c
+++ software/apps/watch/date_time.c
@@ -4,10 +4,21 @@
 #include "date_time.h"
 #include "scheduler_config.h"
 
+// Month names
+static char * month_strings[12] = {
+    "jan", "feb", "mar", "apr", "may", "jun",
+    "jul", "aug", "sep", "oct", "nov", "dec"
+ };
+
+// Day names
+static char * day_strings[7] = {
+    "sun", "mon", "tue", "wed", "thu", "fri", "sat"    
+};
+
 // Stores the callback
 static void (*on_min_hr_change)(void);
 
-// TODO use a table instead
+
 /**
  * http://stackoverflow.com/questions/6385190/
  *                  correctness-of-sakamotos-algorithm-to-find-the-day-of-week
@@ -19,71 +30,11 @@ static void update_day_month_str()
     static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4}; 
 
     year = date_time.year_2digit;   
-    year += 2000 - (date_time.month_num < DATE_MAR_NUM);
+    year += 2000 - (date_time.month_num < 3);
     day_of_week_num = (year + year/4 - year/100 + year/400 +
                        t[date_time.month_num-1] + date_time.day_num) % 7;
-    switch (day_of_week_num) {
-        case DATE_SUN_NUM:    
-            date_time.day_str = DATE_SUN_STR;
-            break; 
-        case DATE_MON_NUM:    
-            date_time.day_str = DATE_MON_STR;
-            break; 
-        case DATE_TUE_NUM:    
-            date_time.day_str = DATE_TUE_STR;
-            break; 
-        case DATE_WED_NUM:    
-            date_time.day_str = DATE_WED_STR;
-            break; 
-        case DATE_THU_NUM:    
-            date_time.day_str = DATE_THU_STR;
-            break; 
-        case DATE_FRI_NUM:    
-            date_time.day_str = DATE_FRI_STR;
-            break; 
-        case DATE_SAT_NUM:    
-            date_time.day_str = DATE_SAT_STR;
-            break; 
-    }
-   
-    switch (date_time.month_num) {
-        case DATE_JAN_NUM:    
-            date_time.month_str = DATE_JAN_STR;
-            break; 
-        case DATE_FEB_NUM:    
-            date_time.month_str = DATE_FEB_STR;
-            break; 
-        case DATE_MAR_NUM:    
-            date_time.month_str = DATE_MAR_STR;
-            break; 
-        case DATE_APR_NUM:    
-            date_time.month_str = DATE_APR_STR;
-            break; 
-        case DATE_MAY_NUM:    
-            date_time.month_str = DATE_MAY_STR;
-            break; 
-        case DATE_JUN_NUM:    
-            date_time.month_str = DATE_JUN_STR;
-            break; 
-        case DATE_JUL_NUM:    
-            date_time.month_str = DATE_JUL_STR;
-            break; 
-        case DATE_AUG_NUM:    
-            date_time.month_str = DATE_AUG_STR;
-            break; 
-        case DATE_SEP_NUM:    
-            date_time.month_str = DATE_SEP_STR;
-            break; 
-        case DATE_OCT_NUM:    
-            date_time.month_str = DATE_OCT_STR;
-            break; 
-        case DATE_NOV_NUM:    
-            date_time.month_str = DATE_NOV_STR;
-            break; 
-        case DATE_DEC_NUM:    
-            date_time.month_str = DATE_DEC_STR;
-            break; 
-    }
+    date_time.day_str = day_strings[day_of_week_num];
+    date_time.month_str = month_strings[date_time.month_num-1];
 }
 
 void date_time_init(void (*on_minute_hour_change)(void))
@@ -96,7 +47,7 @@ void date_time_init(void (*on_minute_hour_change)(void))
     date_time.minutes = 59; 
     date_time.seconds = 55; 
     date_time.day_num = 9;
-    date_time.month_num = DATE_DEC_NUM;
+    date_time.month_num = 12;
     date_time.year_2digit = 15;
     update_day_month_str();
 }
@@ -117,33 +68,33 @@ void date_time_increment_second()
     }                                                                           
     if (day_update) {
         switch (date_time.month_num) {
-            case DATE_JAN_NUM:
-            case DATE_MAR_NUM:
-            case DATE_MAY_NUM:
-            case DATE_JUL_NUM:
-            case DATE_AUG_NUM:
-            case DATE_OCT_NUM:
+            case 1:  // Jan
+            case 3:  // Mar
+            case 5:  // May 
+            case 7:  // July
+            case 8:  // Aug
+            case 10: // Oct 
                 if (++date_time.day_num > 31) {
                     date_time.day_num = 0;
                     ++date_time.month_num;
                 }                
                 break;
-            case DATE_DEC_NUM:
+            case 12: // Dec
                 if (++date_time.day_num > 31) {
                     date_time.day_num = 0;
-                    date_time.month_num = DATE_JAN_NUM;
+                    date_time.month_num = 1; // Jan
                 }                
                 break;
-            case DATE_APR_NUM:
-            case DATE_JUN_NUM:
-            case DATE_SEP_NUM:
-            case DATE_NOV_NUM:
+            case 4:  // Apr
+            case 6:  // Jun
+            case 9:  // Sep
+            case 11: // Nov
                 if (++date_time.day_num > 30) {
                     date_time.day_num = 0;
                     ++date_time.month_num;
                 }                
                 break; 
-            case DATE_FEB_NUM:
+            case 2: // Feb
                 if (date_time.year_2digit % 4) { // Leap year
                     max_days_feb = 28;
                 }
--- software/apps/watch/date_time.h
+++ software/apps/watch/date_time.h
@@ -6,48 +6,6 @@
 
 #include <stdint.h>
 
-#define DATE_JAN_NUM 1
-#define DATE_FEB_NUM 2
-#define DATE_MAR_NUM 3
-#define DATE_APR_NUM 4
-#define DATE_MAY_NUM 5
-#define DATE_JUN_NUM 6
-#define DATE_JUL_NUM 7
-#define DATE_AUG_NUM 8
-#define DATE_SEP_NUM 9
-#define DATE_OCT_NUM 10
-#define DATE_NOV_NUM 11
-#define DATE_DEC_NUM 12
-
-#define DATE_JAN_STR "jan"
-#define DATE_FEB_STR "feb"
-#define DATE_MAR_STR "mar"
-#define DATE_APR_STR "apr"
-#define DATE_MAY_STR "may"
-#define DATE_JUN_STR "jun"
-#define DATE_JUL_STR "jul"
-#define DATE_AUG_STR "aug"
-#define DATE_SEP_STR "sep"
-#define DATE_OCT_STR "oct"
-#define DATE_NOV_STR "nov"
-#define DATE_DEC_STR "dec"
-
-#define DATE_SUN_NUM 0
-#define DATE_MON_NUM 1
-#define DATE_TUE_NUM 2
-#define DATE_WED_NUM 3
-#define DATE_THU_NUM 4
-#define DATE_FRI_NUM 5
-#define DATE_SAT_NUM 6
-
-#define DATE_SUN_STR "sun" 
-#define DATE_MON_STR "mon"
-#define DATE_TUE_STR "tue"
-#define DATE_WED_STR "wed"
-#define DATE_THU_STR "thu"
-#define DATE_FRI_STR "fri"
-#define DATE_SAT_STR "sat"
-
 
 typedef struct date_time_struct {
     // Date
