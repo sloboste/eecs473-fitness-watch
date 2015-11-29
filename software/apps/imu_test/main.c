1e65174d7d47b1bf6cf03df3677673cfdd86e73c
sloboste <sloboste@umich.edu>
2015-11-29 18:38:56 -0500

    use bluetooth and led to show step count.

--- software/apps/imu_test/main.c
+++ software/apps/imu_test/main.c
@@ -1,10 +1,10 @@
-/* Test app for the mpu step counter. This will get the step count and display
- * it to the screen. 
+/* Test application for bluetooth functionality.
  * 
- * MAKE SURE THE PIN ASSIGNMENTS ARE CORRECT IN 
- * software/nrf5x-base/sdk/nrf51_sdk_9.9.0/components/drivers_nrf/twi_master/incubated/config/twi_master_config.h
- * AS WELL AS IN
- * software/lib/lcd/spi_driver.h
+ * This application will continuously advertise (if not connected). When
+ * connected, it will offer the same BLE service as the watch app, but with
+ * fake data (see lib/ble/packets.h and lib/ble/watch_service.h for a
+ * description of how it works). One of the LED's on the board should blink at
+ * 1 Hz and the other will flash for 100ms whenever a request is handled.
  */
 
 #include <stdbool.h>
@@ -15,17 +15,39 @@
 #include "nrf.h"
 #include "softdevice_handler.h"
 
-#include "nrf_delay.h"
 #include "nrf_gpio.h"
+
+#include "timer_config.h"
+#include "scheduler_config.h"
+
+#include "ble_config.h"
+#include "watch_service.h"
+#include "packets.h"
+
+#include "pcb.h"
+//#include "blue_dev_board.h"
+
+#include "nrf_delay.h"
 
 #include "mpu.h"
 
-#include "spi_driver.h"
-#include "lcd_driver.h"
+static uint8_t battery_level = 0;
+static uint16_t heart_rate_bpm = 0;
+static uint32_t step_count = 0;
 
-//#include "blue_dev_board.h"
-#include "pcb.h"
+static void on_adv_con_change(uint8_t ble_state)
+{
 
+}
+
+void pcb_gpio_init(void)
+{
+    nrf_gpio_cfg_output(PIN_LED_1);
+    nrf_gpio_cfg_output(PIN_LED_2);
+
+    nrf_gpio_pin_set(PIN_LED_1);
+    nrf_gpio_pin_set(PIN_LED_2);
+}
 
 void app_error_handler(uint32_t error_code, uint32_t line_num,
                        const uint8_t * p_file_name)
@@ -34,32 +56,141 @@ void app_error_handler(uint32_t error_code, uint32_t line_num,
     NVIC_SystemReset();
 }
 
+void task_1hz_1(void * arg_ptr)
+{
+    // Do nothing
+}
+
+void task_10hz(void * arg_ptr)
+{
+    // Do nothing
+}
+
+void task_1hz_0(void * arg_ptr)
+{
+    --battery_level;
+    ++heart_rate_bpm;
+    step_count = get_steps();
+    if (step_count > 10) {
+        nrf_gpio_pin_toggle(PIN_LED_1);
+    } else {
+        nrf_gpio_pin_toggle(PIN_LED_2);
+    }
+}
+
+static ble_watch_request_handler_t request_handler(uint8_t * data, uint16_t len)
+{
+    static uint8_t packet_buf[PACKET_BUF_LEN];
+    uint8_t packet_type;
+    packet_type = packets_decode_request_packet(data, (uint8_t) len);
+
+    char * fake_latitude = "42 17.5400 N";
+    char * fake_longitude = "083 42.8600 W"; 
+    uint32_t fake_speed = 12345; 
+
+    // TODO finish
+    switch (packet_type) {
+        case PACKET_TYPE_REQUEST_PED_STEP_COUNT:
+            // Send step count
+            memset(&packet_buf, 0, PACKET_BUF_LEN);    
+            uint32_t step_count_rev = __REV(step_count);
+            packets_build_reply_packet(
+                packet_buf,
+                PACKET_TYPE_REPLY_PED_STEP_COUNT,
+                (void *) &step_count_rev,
+                sizeof(step_count),
+                true); 
+            ble_watch_send_reply_packet(packet_buf, PACKET_BUF_LEN);
+            break;
+
+        case PACKET_TYPE_REQUEST_GPS_DATA: 
+            // Send latitude
+            memset(&packet_buf, 0, PACKET_BUF_LEN);    
+            packets_build_reply_packet(
+                packet_buf,
+                PACKET_TYPE_REPLY_GPS_LATITUDE,
+                (void *) fake_latitude,
+                sizeof(fake_latitude),
+                true); 
+            ble_watch_send_reply_packet(packet_buf, PACKET_BUF_LEN);
+            // Send longitude
+            memset(&packet_buf, 0, PACKET_BUF_LEN);    
+            packets_build_reply_packet(
+                packet_buf, 
+                PACKET_TYPE_REPLY_GPS_LONGITUDE,
+                (void *) fake_longitude,
+                sizeof(fake_longitude),
+                true);
+            ble_watch_send_reply_packet(packet_buf, PACKET_BUF_LEN);
+            // Send speed
+            uint32_t speed_rev = __REV(fake_speed); 
+            memset(&packet_buf, 0, PACKET_BUF_LEN);    
+            packets_build_reply_packet(
+                packet_buf, 
+                PACKET_TYPE_REPLY_GPS_SPEED,
+                (void *) &speed_rev,
+                sizeof(fake_speed),
+                true);
+            ble_watch_send_reply_packet(packet_buf, PACKET_BUF_LEN);
+            break;
+
+        case PACKET_TYPE_REQUEST_GPS_LOG: 
+            // Send GPS logs
+            // TODO
+            memset(&packet_buf, 0, PACKET_BUF_LEN);    
+            break;
+
+        case PACKET_TYPE_REQUEST_BATTERY_LEVEL:
+            // Send battery level
+            memset(&packet_buf, 0, PACKET_BUF_LEN);    
+            packets_build_reply_packet(
+                packet_buf, 
+                PACKET_TYPE_REPLY_BATTERY_LEVEL,
+                (void *) &battery_level,
+                sizeof(battery_level),
+                true);
+            ble_watch_send_reply_packet(packet_buf, PACKET_BUF_LEN);
+            break;
+
+        case PACKET_TYPE_REQUEST_HEART_RATE:
+            // Send heart rate
+            memset(&packet_buf, 0, PACKET_BUF_LEN);    
+            uint32_t heart_rate_bpm_rev = __REV16(heart_rate_bpm);
+            packets_build_reply_packet(
+                packet_buf, 
+                PACKET_TYPE_REPLY_HEART_RATE,
+                (void *) &heart_rate_bpm_rev,
+                sizeof(heart_rate_bpm_rev),
+                true);
+            ble_watch_send_reply_packet(packet_buf, PACKET_BUF_LEN);
+            break;
+
+        default:
+            // Unrecognized request. Send all 0's
+            memset(&packet_buf, 0, PACKET_BUF_LEN);    
+            ble_watch_send_reply_packet(packet_buf, PACKET_BUF_LEN);
+            break;
+
+    }
+
+    //nrf_gpio_pin_toggle(PIN_LED_2);
+
+    //return 0; // FIXME what is the return value supposed to be? It's not used.
+}
+
 int main(void)
 {
-    uint32_t step_count = 0;
-    
-    nrf_gpio_cfg_output(PIN_LED_1);
-    nrf_gpio_pin_set(PIN_LED_1);
-
-    nrf_gpio_cfg_output(PIN_LED_2);
-    nrf_gpio_pin_set(PIN_LED_2);
+    pcb_gpio_init();
+    timers_init(true);
+    scheduler_init();
 
     mympu_open(200);
 
-    spi_init();
-    lcd_initCursor();
+    ble_init(request_handler, on_adv_con_change);
+    timer_start_1hz_periodic_0();
+    ble_advertising_start();
 
     while (1) {
-        step_count = get_steps(); 
-        if (step_count > 10) {
-            nrf_gpio_pin_toggle(PIN_LED_1);
-        } else {
-            nrf_gpio_pin_toggle(PIN_LED_2);
-        } 
-        lcd_clearDisplay();
-        lcd_setCursor(1, 37);
-        lcd_transferBigNumInt(step_count); 
-        lcd_refresh();
-        nrf_delay_ms(1000);
+        app_sched_execute();
     }
 }
